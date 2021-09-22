b35fbeb72263f01d04fbb11aebf3f4feafd73c57
Palerion <aws4@clemson.edu>
2021-09-22 00:55:04 -0400

    Created launch.json and tasks.json files for C# compatibility.

--- driver_rewards_program/ClientApp/src/App.js
+++ driver_rewards_program/ClientApp/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Helmet from 'react-helmet';
 import EmailForm from './components/EmailForm';
 import PasswordForm from './components/PasswordForm';
 import SelectDriver from './components/SelectDriver';
