b2d303b04dbafe5205ebb611db9aa3706337dcb8
Benjamin Anderson <bta2@clemson.edu>
2021-10-25 16:13:41 +0000

    Updated PageSponsorCatalog.js

--- driver_rewards_program/ClientApp/src/components/PageSponsorCatalog.js
+++ driver_rewards_program/ClientApp/src/components/PageSponsorCatalog.js
@@ -6,33 +6,33 @@ function PageSponsorCatalog (props) {
         <div>
           <NavBarSponsor></NavBarSponsor>
           <div class="columns">
-          <div class="notification is-white py-6 my-6">
-    <div class="column is one-quarter">
-    <p class="menu-label">
+            <div class="notification is-white py-6 my-6">
+              <div class="column is one-quarter">
+                <p class="menu-label">
                   Sponsor Catalog
                 </p>
-      <div class="buttons">
-        <button class="button">Add Item</button>
-      </div>
-      <div class="buttons">
-        <button class="button">Delete Item</button>
-      </div>
-      <div class="buttons">
-        <button class="button">Refresh Items</button>
-      </div>
-    </div>
-  </div>
-    <div class="column is three-quarter">
-    <div class="notification is-white py-6 my-6">
-    <p class="title is-5">Sponsor Catalog</p>
-    </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="box">
-      </div>
-  </div>
-</div>
+                  <div class="buttons">
+                    <button class="button">Add Item</button>
+                  </div>
+                  <div class="buttons">
+                    <button class="button">Delete Item</button>
+                  </div>
+                  <div class="buttons">
+                    <button class="button">Refresh Items</button>
+                  </div>
+              </div>
+            </div>
+            <div class="column is three-quarter">
+              <div class="notification is-white py-6 my-6">
+                <p class="title is-5">Sponsor Catalog</p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="box">
+            </div>
+          </div>
+        </div>
     )
 }
 
