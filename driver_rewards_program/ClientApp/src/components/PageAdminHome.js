e21f0097d6680816efd717be4fdc2f45678dc401
Palerion <aws4@clemson.edu>
2021-11-08 18:58:39 -0500

    verifying local features exist remotely

---
+++ driver_rewards_program/ClientApp/src/components/PageAdminHome.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import TableSponsorDrivers from './TableSponsorDrivers';
+import NavBarSponsor from './NavBarSponsor';
+
+function PageAdminHome () {
+
+  const [driver_ids] = useState(JSON.parse(localStorage.getItem('driversid')));
+  const [driver_firsts] = useState(JSON.parse(localStorage.getItem('driversfirst')));
+  const [driver_lasts] = useState(JSON.parse(localStorage.getItem('driverslast')));
+  const [driver_emails] = useState(JSON.parse(localStorage.getItem('driversemail')));
+  const [driver_points] = useState(JSON.parse(localStorage.getItem('driverspoints')));
+
+    return (
+      <div>
+        <NavBarSponsor></NavBarSponsor>
+        <div class="container">
+          <div class="columns">
+            <div class="column is-3">
+              <div class="notification is-white py-6 my-6">
+                <p class="menu-label">
+                  General
+                </p>
+                <ul class="menu-list">
+                  <li><a>Dashboard</a></li>
+                  <li><a>Customers</a></li>
+                  <li><a>Other</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="column is-9">
+              <div class="notification is-white py-6 my-6">
+                <section class="hero is-a-beautiful-sunset has-rounded-corners welcome is-small">
+                  <div class="hero-body">
+                    <div class="container">
+                      <h1 class="title">
+                        Hello, Sponsor.
+                      </h1>
+                      <h2 class="subtitle">
+                        I hope you are having a great day!
+                      </h2>
+                    </div>
+                  </div>
+                </section>
+                <div class="columns">
+                  <div class="column is-6 py-6">
+                    <TableSponsorDrivers 
+                    id={driver_ids}
+                    first={driver_firsts}
+                    last={driver_lasts}
+                    email={driver_emails}
+                    points={driver_points}>
+                    </TableSponsorDrivers>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default PageAdminHome;
