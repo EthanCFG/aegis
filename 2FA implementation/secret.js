4444fbd805d00f885a112e55e7d89b192cfa7ed2
Kevin Chavez <kchavez@clemson.edu>
2021-11-10 22:48:12 +0000

    Added secret.js

---
+++ 2FA implementation/secret.js
@@ -0,0 +1,15 @@
+//code to generate the secret used in 2FA from article of 2FA
+
+import { generateSecret } from 'speakeasy'
+interface TwoFactorEntity {
+  userId: number
+  secret: string
+  enabled: boolean // default value is false
+}
+const generateUserSecret = 
+(userRepo: Respository, twoFactor: TwoFactorEntity) => {
+const secret = generateSecret()
+   twoFactor.secret = secret.base32
+   userRepo.save(twoFactor)
+   return secret
+}
