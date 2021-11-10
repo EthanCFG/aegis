6059c8dbab46d01cf12f5b877a5478b29c9e79c8
Benjamin Anderson <bta2@clemson.edu>
2021-11-10 00:28:23 +0000

    Updated CatalogItem.js

--- driver_rewards_program/ClientApp/src/components/CatalogItem.js
+++ driver_rewards_program/ClientApp/src/components/CatalogItem.js
@@ -2,17 +2,32 @@ import React from "react";
 
 export default function CatalogItem(item) {
   return (
-    <li class="notification is-kinda-gray py-6 my-6">
-      {/* <img src={this.props.imgsrc} /> */}
-      <img
-        class="catalog-item-image"
-        src={
-          "https://tesla-cdn.thron.com/delivery/public/image/tesla/c82315a6-ac99-464a-a753-c26bc0fb647d/bvlatuR/std/1200x628/lhd-model-3-social"
-        }
-      />
-      <h3 class="catalog-item-text">{item.Catalog_Item_Name}</h3>
-      <p class="catalog-item-text">Price: {item.Catalog_Item_Price}</p>
-      <button class="catalog-item-button">Purchase</button>
-    </li>
+    <div class="card">
+  <div class="card-image">
+        <figure class="card-image has-text-centered px-6">
+      <img src="https://tesla-cdn.thron.com/delivery/public/image/tesla/c82315a6-ac99-464a-a753-c26bc0fb647d/bvlatuR/std/1200x628/lhd-model-3-social" alt="Placeholder image">
+    </img>
+        </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+        </img>
+            </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">{item.Catalog_Item_Name}</p>
+        <p class="subtitle is-6">{item.Catalog_Item_Price}</p>
+      </div>
+    </div>
+
+        <div class="content">
+        <button class="catalog-item-button">Purchase</button>
+      <div> </div>
+    </div>
+  </div>
+</div>
   );
 }
